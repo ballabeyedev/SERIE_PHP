b5eb5696f751494b9d2ae794c54db26c7818ed4d
ballabeyedev <151043765+ballabeyedev@users.noreply.github.com>
2023-12-19 02:50:59 +0000

    Update exercice16.php
    Mon_code

--- exercice16.php
+++ exercice16.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        .result {
+            margin-top: 20px;
+            font-weight: bold;
+        }
+    </style>
+    <title>Trigonometry Calculator</title>
+</head>
+<body>
+
+<?php
+
 function calculerSinus($angle, $unite = 'radian') {
     switch ($unite) {
         case 'degre':
@@ -16,13 +37,17 @@ function calculerSinus($angle, $unite = 'radian') {
 // Exemples d'utilisation de la fonction
 $angleEnRadian = 90;
 $sinusEnRadian = calculerSinus($angleEnRadian);
-echo "Sinus de l'angle en radians : $sinusEnRadian\n";
+echo "<p class='result'>Sinus de l'angle en radians : $sinusEnRadian</p>";
 
 $angleEnDegre = 90; 
 $sinusEnDegre = calculerSinus($angleEnDegre, 'degre');
-echo "Sinus de l'angle en degrés : $sinusEnDegre\n";
+echo "<p class='result'>Sinus de l'angle en degrés : $sinusEnDegre</p>";
 
 $angleEnGrade = 90; // Remplacez cela par votre valeur d'angle en grades
 $sinusEnGrade = calculerSinus($angleEnGrade, 'grade');
-echo "Sinus de l'angle en grades : $sinusEnGrade\n";
+echo "<p class='result'>Sinus de l'angle en grades : $sinusEnGrade</p>";
 
+?>
+
+</body>
+</html>
